6c6f790c2fa45eac9d0395ade649a4485b598080
Charlotte Becconsall <charlottebecconsall@gmail.com>
2023-12-02 00:23:37 +1300

    Day 1 part 1 (in C)

---
+++ Day 1/C/PartOne.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    // Open and read the file
+    char *filepath = "..\\input.txt";
+    FILE *input_file = fopen(filepath, "r");
+    char input_line[128];
+    int sum = 0;
+    while (fgets(input_line, 128, input_file) != NULL) {
+        int calibration_value = 0;
+        int last_digit = 0;
+        for (int i=0; input_line[i] != '\0'; i++) {
+            if (isdigit(input_line[i])) {
+                last_digit = input_line[i] - '0';
+                if (calibration_value == 0) {
+                    calibration_value = (last_digit * 10);
+                }
+            }
+        }
+        sum = sum + calibration_value + last_digit;
+    }
+
+    printf("\n%d", sum);
+
+    fclose(input_file);
+    return 0;
+}

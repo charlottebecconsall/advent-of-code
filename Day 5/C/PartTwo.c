fee78c8725bee434e0bfd53d57a7f673096b962f
Charlotte Becconsall <charlottebecconsall@gmail.com>
2023-12-07 00:26:31 +1300

    yos

---
+++ Day 5/C/PartTwo.c
@@ -0,0 +1,70 @@
+/*
+Very confusing farming
+Also I haven't done this yet, I just have the file setup for it
+*/
+
+#include <stdio.h>
+#include <ctype.h>
+
+// #define NUM_SEEDS 4
+#define MAPPINGS_SIZE 3
+
+#define NUM_SEEDS 20
+
+int get_numbers(char *input_line, long long *return_array, int return_array_len)
+{
+    long long number = 0;
+    int counter = 0;
+    int found_numbers = 0;
+    for (int i=0; input_line[i] != '\n'; i++) {
+        if (isdigit(input_line[i])) {
+            found_numbers = 1;
+            number *= 10;
+            number += input_line[i] - '0';
+            if ((input_line[i+1] == ' ') || (input_line[i+1] == '\n')) {
+                return_array[counter] = number;
+                number = 0;
+                counter++;
+            }
+        } 
+
+    }
+    if (found_numbers == 0) {
+        for (int i=0; i < return_array_len; i++) {
+            return_array[i] = 0;
+        }
+    }
+    return found_numbers;
+}
+
+
+int main(void) 
+{
+    char *filepath = "..\\input.txt";
+    FILE *input_file = fopen(filepath, "r");
+    char input_line[256];
+    long long seeds[NUM_SEEDS];
+    int seeds_padlock[NUM_SEEDS] = {0};
+    long long mappings[MAPPINGS_SIZE];
+    long long result = 0;
+
+    fgets(input_line, 256, input_file);    // Initialises seeds
+    get_numbers(input_line, seeds, NUM_SEEDS);
+    while (fgets(input_line, 256, input_file) != NULL) {
+        int found_numbers = get_numbers(input_line, mappings, MAPPINGS_SIZE);
+        for (int i=0; i < NUM_SEEDS; i++) {
+            if ((mappings[1] <= seeds[i]) && (seeds[i] <= (mappings[1] + mappings[2])) && seeds_padlock[i] == 0) { 
+                long long offset = seeds[i] - mappings[1];
+                seeds[i] = mappings[0] + offset;
+                seeds_padlock[i] = 1;
+            }
+        }
+        if (found_numbers == 0) {for (int i=0; i<NUM_SEEDS; i++) {seeds_padlock[i] = 0;}}
+    }
+    result = seeds[0];
+    for (int i=0; i < NUM_SEEDS; i++) {
+        if (seeds[i] < result) {result = seeds[i];}
+    }
+    printf("%lld,", result);
+    
+}
---
+++ Day 6/C/PartOne.c
@@ -0,0 +1,72 @@
+/*
+We racing boats now
+*/
+
+#include <stdio.h>
+#include <math.h>
+#include <ctype.h>
+
+// #define NUM_RACES 3
+#define NUM_RACES 4
+
+struct boat_race 
+{
+    int time;
+    int distance;
+};
+
+
+void get_boat_race_info(struct boat_race *races, char *time_info, char *distance_info) 
+{
+    int time = 0;
+    int distance = 0;
+    int counter = 0;
+    for (int i=0; distance_info[i] != '\n'; i++) {
+        if (isdigit(time_info[i])) {
+            time *= 10;
+            time += time_info[i] - '0';
+            if (time_info[i+1] == ' ' || distance_info[i+1] == '\n') {
+                races[counter].time = time;
+                time = 0;
+            }
+        }
+        if (isdigit(distance_info[i])) {
+            distance *= 10;
+            distance += distance_info[i] - '0';
+            if (distance_info[i+1] == ' ' || distance_info[i+1] == '\n') {
+                races[counter].distance = distance;
+                distance = 0;
+                counter++;
+            }
+        }
+    }
+}
+
+
+int main(void)
+{
+    char *filepath = "..\\input.txt";
+    FILE *input_file = fopen(filepath, "r");
+    char time_info[128];
+    char distance_info[128];
+    int result = 1;
+    struct boat_race races[NUM_RACES];
+
+    fgets(time_info, 128, input_file);
+    fgets(distance_info, 128, input_file);
+
+    get_boat_race_info(races, time_info, distance_info);
+
+    for (int i=0; i < NUM_RACES; i++) {
+        float d = (races[i].time * races[i].time) - (4 * 1 * (races[i].distance + 1));
+        int low_num = ceil((-races[i].time + sqrt(d)) / -2);
+        int high_num = floor((-races[i].time - sqrt(d)) / -2);
+        printf("Low num: %d\n", low_num);
+        printf("High num: %d\n", high_num);
+        result *= high_num - low_num + 1;
+    }
+
+    printf("%d", result);
+    
+    return 0;
+}
